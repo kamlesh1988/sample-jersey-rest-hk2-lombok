86e122cff37ca137d4b5b95d1232cc9cfe4e7fc2
shubham.rai <shubham.rai@gupshup.io>
2019-02-18 10:45:48 +0530

    Resolved the compilation errors and test failures

--- src/main/java/com/kamlesh/poc/jersey/service/TestService.java
+++ src/main/java/com/kamlesh/poc/jersey/service/TestService.java
@@ -4,14 +4,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.inject.Inject;
 
+import com.kamlesh.poc.jersey.di.MyDiscoverableFeature;
 import org.jvnet.hk2.annotations.Service;
 
-import com.kamlesh.poc.jersey.DependencyResolver;
+import com.kamlesh.poc.jersey.di.DependencyResolver;
 import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 @Log4j2
 public class TestService {
+	private static final Logger logger = LoggerFactory.getLogger(MyDiscoverableFeature.class.getName());
 
 	private static AtomicInteger count = new AtomicInteger(0);
 
@@ -19,7 +23,7 @@ public class TestService {
 	DependencyResolver resolver;
 
 	public TestService() {
-		log.info("Constructor called count {}", count.incrementAndGet());
+		logger.info("Constructor called count {}", count.incrementAndGet());
 	}
 
 	public boolean doTask() {
